26828a80efc290ac0a0c19f4c3d95a160e00c7b7
Felipe Buiras <f.morisan@gmail.com>
2024-08-07 18:32:40 -0300

    feat: add references to viridian advisor

--- src/config/app.ts
+++ src/config/app.ts
@@ -11,13 +11,13 @@ interface AppConfig {
 }
 
 export const appConfig: AppConfig = {
-    name: "Where is all the sDAI?",
+    name: "Where is all the sDAI? - Viridian Advisor",
     github: {
         title: "Where is all the sDAI?",
         url: "https://github.com/fmorisan/where-is-the-sdai",
     },
     author: {
-        name: "fmorisan, with thanks to hayyi2",
+        name: "Viridian Advisor & fmorisan, with thanks to hayyi2",
         url: "https://github.com/fmorisan/",
     }
 }
